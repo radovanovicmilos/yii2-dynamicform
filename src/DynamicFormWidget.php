5e7e6e8e1d6c23307b746f1902c43d1bfba6e1c7
Igor Jerosimić <igor@web-labs.rs>
2018-07-19 10:53:21 +0200

    Revert "Namespace"
    This reverts commit fe1782bfe915ba13e4bd30b5d8211a5adc1cbb2d.

--- src/DynamicFormWidget.php
+++ src/DynamicFormWidget.php
@@ -5,7 +5,7 @@
  * @license   https://github.com/wbraganca/yii2-dynamicform/blob/master/LICENSE
  */
 
-namespace igorcode\dynamicform;
+namespace wbraganca\dynamicform;
 
 use Yii;
 use Symfony\Component\DomCrawler\Crawler;
